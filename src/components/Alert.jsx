ca7c734b2c80004590a56952d31cb01720dc0f9e
jorge2101719 <rusosky01@gmail.com>
2023-08-14 03:48:05 -0400

    se agrega estilo al mensaje de Alert

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,7 +1,9 @@
+// import 'react-bootstrap/dist/css/bootstrap.min.css'
+
 const Alert = ({datosPadre}) => {
   return (
     <>
-      <div>
+      <div className="bg-success text-light m-1 p-2 rounded-pill">
         {datosPadre}
       </div>
     </>
